03c2ad4e3a92c7973b15fcd646c20244d5f28d56
Hugo Morales <moralesbatzh@gmail.com>
2023-09-13 12:00:04 -0600

    pdf

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php 
 require_once __DIR__ . '/../includes/app.php';
 
-
+use Controller\ReporteController;
 use MVC\Router;
 use Controllers\AppController;
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [AppController::class,'index']);
+$router->get('/pdf', [ReporteController::class,'pdf']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
